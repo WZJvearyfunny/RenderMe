a4aec01d6d7fb0915877f6b532185e24b13c6dff
WZJvearyfunny <824940937@qq.com>
2021-09-28 21:04:24 +0800

    首页完善

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <el-container class="sign-page_sty">
-    <el-header class="sign-page_header">
+  <el-container class="page_sty">
+    <el-header class="page_header" v-if="false">
       <img
         src="https://iconfont.alicdn.com/t/7862a342-1387-4589-9a23-5ec8bccbe68d.png"
         width="60px"
         style="border-radius: 30px"
       />
       <el-menu
-        class="sign-page_header_menu"
+        class="page_header_menu"
         mode="horizontal"
         @select="handleSelect"
       >
@@ -30,7 +30,7 @@
           </el-submenu>
         </el-submenu>
         <el-menu-item index="3" disabled>消息中心</el-menu-item>
-        <div class="sign-page_menu_button">
+        <div class="page_menu_button">
           <el-button size="small" @click="routeJump('0')">Sign Up</el-button>
           <el-button size="small" type="primary" @click="routeJump('1')"
             >Sign In</el-button
@@ -39,6 +39,41 @@
       </el-menu>
     </el-header>
     <el-main>
+      <div class="homepage-main_top">
+        <div style="width: 90%">
+          <div class="homepage-main_top_space">
+            <div class="homepage-main_top_space_center">
+              <h1 class="homepage-main_top_space_center_title">
+                Where the world builds software
+              </h1>
+              <p class="homepage-main_top_space_center_dec">
+                Millions of developers and companies build, ship, and maintain
+                their software on GitHub—the largest and most advanced
+                development platform in the world.
+              </p>
+              <div class="homepage-main_top_space_center_sign">
+                <el-input
+                  placeholder="Email address"
+                  v-model="emailAddress"
+                  clearable
+                >
+                </el-input>
+                <el-button type="success" size="medium">
+                  Sign up for GitHub
+                </el-button>
+              </div>
+            </div>
+            <div></div>
+          </div>
+          <el-divider class="homepage-main_top_divider"></el-divider>
+          <div class="homepage-main_top_tag">
+            <div v-for="(item, index) in topTag" v-bind:key="index">
+              <div class="homepage-main_top_tag__title">{{ item.title }}</div>
+              <div class="homepage-main_top_tag__dec">{{ item.desc }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
       <HomeCard
         imgsrcset="https://github.githubassets.com/images/modules/site/home/enterprise-city-w-logos.webp"
         imgsrc="https://github.githubassets.com/images/modules/site/home/enterprise-city-w-logos.jpg"
@@ -46,7 +81,19 @@
         pdec="Take collaboration to the next level with security and administrative features built for teams."
         :buttonList="buttonList"
         @routeJump="routeJump"
+        ref="homecard"
       />
+      <div class="homepage-main_actmenu">
+        <div class="homepage-main_actmenu__grid">
+          <div>Code</div>
+          <div>Collaborate</div>
+          <div>Develop</div>
+          <div>Automate</div>
+          <div>Secure</div>
+          <div>Community</div>
+        </div>
+      </div>
+      <div style="width: 100%; height: 1080px">123</div>
     </el-main>
     <el-footer>I'm studying now,xx!</el-footer>
   </el-container>
@@ -68,7 +115,35 @@ export default {
           contact: "Contact Sales",
         },
       ],
+      emailAddress: "",
+      topTag: [
+        {
+          title: "65+ million",
+          desc: "Developers",
+        },
+        {
+          title: "3+ million",
+          desc: "Organizations",
+        },
+        {
+          title: "200+ million",
+          desc: "Repositories",
+        },
+        {
+          title: "72%",
+          desc: "Fortune 50",
+        },
+      ],
     };
+  },
+  mounted() {
+    const that = this;
+    document
+      .getElementsByClassName("el-main")[0]
+      .addEventListener("scroll", function () {
+        console.log("页面滚动事件");
+        console.dir(that.$refs.homecard.$el);
+      });
   },
   methods: {
     handleSelect(index, indexPath) {
@@ -79,26 +154,103 @@ export default {
       console.log(signtype);
       this.$router.push({ path: "sign", query: { signtype } });
     },
+    getScroll() {
+      console.log("112233");
+    },
   },
 };
 </script>
 <style scoped>
-.sign-page_sty {
+.page_sty {
   width: 100vw;
   height: 100vh;
 }
-.sign-page_header {
+.page_header {
   display: flex;
   justify-content: center;
 }
-.sign-page_header_menu {
-  width: 80%;
+.page_header_menu {
+  width: 90%;
 }
-.sign-page_menu_button {
+.page_menu_button {
   position: absolute;
   right: 0;
   display: flex;
   align-items: center;
   height: 100%;
+}
+.homepage-main_top {
+  width: 100%;
+  height: 690px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.homepage-main_top_space {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  height: 524px;
+}
+.homepage-main_top_tag {
+  width: 80%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+}
+.homepage-main_top_tag__title {
+  font-size: 20px !important;
+  line-height: 28px !important;
+  text-align: left;
+}
+.homepage-main_top_tag__dec {
+  color: rgba(0, 0, 0, 0.7) !important;
+  font-size: 12px !important;
+  line-height: 20px !important;
+  text-align: left;
+}
+.homepage-main_top_space_center {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+.homepage-main_top_space_center_title {
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 72px !important;
+  line-height: 76px !important;
+  text-align: left;
+  color: #666;
+}
+.homepage-main_top_space_center_dec {
+  font-size: 24px !important;
+  line-height: 32px !important;
+  text-align: left;
+  color: #8193b2 !important;
+}
+.homepage-main_top_space_center_sign {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-column-gap: 20px;
+  margin-right: 15%;
+}
+.homepage-main_actmenu {
+  margin-top: 5%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: sticky;
+  top: 0px;
+}
+.homepage-main_actmenu__grid {
+  width: 90%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr;
+  color: #24292f;
+  font-size: 16px !important;
+  line-height: 24px !important;
+}
+>>> .el-button--success {
+  color: #fff;
+  background-color: #54a459;
+  border-color: #54a459;
 }
 </style>
